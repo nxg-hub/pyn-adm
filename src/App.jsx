4ecea1cf1e32dce09538b3a16067da737595c10f
Queen <samuellqueenie@gmail.com>
2025-03-07 15:34:39 +0100

    added signup and login pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 import { Route, Routes } from "react-router-dom";
 import Login from './pages/Login'
 import SignUp from "./pages/Signup";
+import Dashboard from "./pages/Dashboard/Index";
+import Users from "./pages/Dashboard/routes/users/Users";
+import Accounts from "./pages/Dashboard/routes/Accounts/Accounts";
+import Home from "./pages/Dashboard/routes/Home/Home";
+
 function App() {
   return (
     <div>
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/SignUp" element={<SignUp />} />
+        <Route exact path="/Dashboard" element={<Dashboard />}>
+          <Route path="/Dashboard" element={<Home />} />
+          <Route path="/Dashboard/Users" element={<Users />} />
+          <Route path="/Dashboard/Accounts" element={<Accounts />} />
+        </Route>
       </Routes>
     </div>
   );
